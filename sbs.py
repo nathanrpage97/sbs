50eb9128c4b0c3c77d7efea9c3213afa2b3fa595
Nathan Page <nathanrpage97@gmail.com>
2022-05-14 15:42:44 -0700

    correct capturing raw bytes

--- sbs.py
+++ sbs.py
@@ -3,12 +3,14 @@ from typing import Union
 
 libsbs = ct.CDLL("libsbs.so")
 
+
 class sbs(ct.Structure):
     size: int
+    len: int
     _fields_ = [
         ("_str", ct.c_void_p),
-        ("_len", ct.c_size_t),
-        ("size", ct.c_size_t)
+        ("len", ct.c_size_t),
+        ("size", ct.c_size_t),
     ]
 
     @property
@@ -17,13 +19,18 @@ class sbs(ct.Structure):
 
     @property
     def bytes(self) -> bytes:
-        return ct.cast(self._str, ct.POINTER(ct.c_char * self._len)).contents.value
+        return ct.cast(self._str, ct.POINTER(ct.c_char * self.len)).contents.raw
+
+
+a = (ct.c_char * 10)()
 
 
 class SBSException(Exception):
     pass
 
+
 # text = Union[str, bytes]
+
 
 def errcheck(result: int, *args) -> int:
     if result != 0:
@@ -32,26 +39,40 @@ def errcheck(result: int, *args) -> int:
 
 
 libsbs.sbsnewlen.restype = sbs
+
+
 def sbsnewlen(text: bytes, *, size: int) -> sbs:
     buffer = ct.create_string_buffer(size)
-    val = libsbs.sbsnewlen(text, ct.c_size_t(len(text)), ct.byref(buffer), ct.c_size_t(size))
-    val._buffer = buffer # store to prevent garbage collection
+    val = libsbs.sbsnewlen(
+        text, ct.c_size_t(len(text)), ct.byref(buffer), ct.c_size_t(size)
+    )
+    val._buffer = buffer  # store to prevent garbage collection
     return val
+
 
 libsbs.sbsnew.restype = sbs
+
+
 def sbsnew(text: str, *, size: int) -> sbs:
     buffer = ct.create_string_buffer(size)
-    val = libsbs.sbsnewlen(text.encode(), ct.c_size_t(len(text)), ct.byref(buffer), ct.c_size_t(size))
-    val._buffer = buffer # store to prevent garbage collection
+    val = libsbs.sbsnewlen(
+        text.encode(), ct.c_size_t(len(text)), ct.byref(buffer), ct.c_size_t(size)
+    )
+    val._buffer = buffer  # store to prevent garbage collection
     return val
 
+
 libsbs.sbscatlen.errcheck = errcheck
+
+
 def sbscatlen(s: sbs, text: bytes) -> sbs:
     libsbs.sbscatlen(ct.byref(s), text, ct.c_size_t(len(text)))
     return s
 
+
 libsbs.sbscat.errcheck = errcheck
+
+
 def sbscat(s: sbs, text: str) -> sbs:
     libsbs.sbscat(ct.byref(s), text.encode())
     return s
-
--- test_sbs.py
+++ test_sbs.py
@@ -9,8 +9,8 @@ def test_sbsnew() -> None:
 def test_sbscatlen() -> None:
     text = sbs.sbsnew("test", size=18)
     text = sbs.sbscatlen(text, b"test\x00cont")
-    # assert(text.bytes == b"testtest\x00cont")
-
+    # assert text.bytes == b"testtest\x00cont"
+    assert text.len == len(b"testtest\x00cont")
     with pytest.raises(sbs.SBSException):
         sbs.sbscatlen(text, b"really long text that overflows")
 
@@ -18,7 +18,6 @@ def test_sbscatlen() -> None:
 def test_sbscat() -> None:
     text = sbs.sbsnew("test", size=12)
     text = sbs.sbscat(text, "test")
-
     assert text.str == "testtest"
     assert text.size == 12
 
